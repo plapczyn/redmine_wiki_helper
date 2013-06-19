4f9e0c3fb0c578ecf9d4161648ddeb4d5aa10e10
Philip Lapczynski <philip.lapczynski@gmail.com>
2013-06-19 16:17:10 -0400

    Changes to handle links for the pdb protocal.

--- init.rb
+++ init.rb
@@ -1,18 +1,18 @@
 require 'redmine'
 
-class UncHelper
+class PdbHelper
   def initialize
-    @head = /^\\\\/
+    @head = /^[0-9]{10}/
   end
 
-  def is_unc?(str)
+  def is_pdb?(str)
     (str =~ @head) != nil
   end
 
-  def unc_to_file_proto(str)
-    Rails.logger.info "str == #{str}, is_unc? == #{is_unc?(str)}, head=#{@head.to_s}"
-    return "" if !is_unc?(str)
-    str.gsub(@head, "file://///").gsub(/\\/, "/")
+  def pdb_to_proto(str)
+    Rails.logger.info "str == #{str}, is_pdb? == #{is_pdb?(str)}, head=#{@head.to_s}"
+    return "" if !is_pdb?(str)
+    "pdb://" + str
   end
 
   def trim(str)
@@ -21,39 +21,39 @@ class UncHelper
   end
 
   def parse_args(args)
-    unc = trim(args[0])
-    label = trim(args[1]) || unc
+    pdb = trim(args[0])
+    label = trim(args[1]) || pdb
 
-    return unc, label
+    return pdb, label
   end
 
   def get_tag(args)
-    return "(No parameters are specified. A UNC path is needed at least.)" if args.empty?
-    unc, label = parse_args(args)
+    return "(No parameters are specified. A project database ID is needed at least.)" if args.empty?
+    pdb, label = parse_args(args)
 
     return <<TEMPLATE
-<a href=\"#{unc_to_file_proto(unc)}\">#{label}</a>
+<a href=\"#{pdb_to_proto(pdb)}\">#{label}</a>
 TEMPLATE
   end
 
 end
 
-Redmine::Plugin.register :redmine_wiki_unc do
-  name 'Redmine Wiki Unc plugin'
-  author 'Takashi Oguma'
+Redmine::Plugin.register :redmine_wiki_pdb do
+  name 'Redmine Wiki Pdb plugin'
+  author 'Philip Lapczynski'
   description 'This is a plugin for macro of Redmine Wiki'
-  version '0.0.3'
+  version '0.0.1'
 
   Redmine::WikiFormatting::Macros.register do
     desc <<DESC
-Makes a link to UNC path.
+Makes a link to project database path.
 How to use:
-1) without a label: {{unc(\\\\server\\path\\to\\file)}}
-2) with a label:    {{unc(\\\\server\\path\\to\\file, My Secret Document)}}
+1) without a label: {{pdb(ID)}}
+2) with a label:    {{pdb(ID, Description)}}
 DESC
 
-    macro :unc do |obj, args|
-      h = UncHelper.new
+    macro :pdb do |obj, args|
+      h = PdbHelper.new
       h.get_tag(args).html_safe
     end
   end
