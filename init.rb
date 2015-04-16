01037b5085e3d21dedd6acb5eec0088096a14bc3
Philip Lapczynski <philip.lapczynski@gmail.com>
2015-04-16 13:19:00 -0400

    Updated to support ALMplus links

--- init.rb
+++ init.rb
@@ -37,12 +37,48 @@ TEMPLATE
   end
 
 end
+class AlmHelper
+  def initialize
+    @head = /^alm:/
+  end
 
-Redmine::Plugin.register :redmine_wiki_pdb do
-  name 'Redmine Wiki Pdb plugin'
+  def is_alm?(str)
+    (str =~ @head) != nil
+  end
+
+  def alm_to_proto(str)
+    Rails.logger.info "str == #{str}, is_alm? == #{is_alm?(str)}, head=#{@head.to_s}"
+    return "" if !is_alm?(str)
+  end
+
+  def trim(str)
+    return str.strip unless str == nil
+    return nil
+  end
+
+  def parse_args(args)
+    alm = trim(args[0])
+    label = trim(args[1]) || alm
+
+    return alm, label
+  end
+
+  def get_tag(args)
+    return "(No parameters are specified. An ALMplus URI is needed at least.)" if args.empty?
+    alm, label = parse_args(args)
+
+    return <<TEMPLATE
+<a href=\"#{alm_to_proto(alm)}\">#{label}</a>
+TEMPLATE
+  end
+
+end
+
+Redmine::Plugin.register :redmine_wiki_helper do
+  name 'Redmine Wiki Helper Plugin'
   author 'Philip Lapczynski'
-  description 'This is a plugin for macro of Redmine Wiki'
-  version '0.0.1'
+  description 'This is a plugin for extending Redmine macro syntax'
+  version '1.0.0'
 
   Redmine::WikiFormatting::Macros.register do
     desc <<DESC
@@ -50,10 +86,20 @@ Makes a link to project database path.
 How to use:
 1) without a label: {{pdb(ID)}}
 2) with a label:    {{pdb(ID, Description)}}
+
+Makes a link to ALMplus URIs.
+How to use:
+1) without a label: {{alm(ALMplus Link)}}
+2) with a label:    {{alm(ALMplus Link, Description)}}
 DESC
 
     macro :pdb do |obj, args|
       h = PdbHelper.new
+      h.get_tag(args).html_safe
+    end
+
+    macro :alm do |obj, args|
+      h = AlmHelper.new
       h.get_tag(args).html_safe
     end
   end
